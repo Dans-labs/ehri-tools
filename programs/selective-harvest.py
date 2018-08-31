6a06e5c93da4c469f34aacf33b6e6096615c97c6
Dirk Roorda <dirk.roorda@dans.knaw.nl>
2018-08-31 15:14:40 +0200

    selective harvesting

--- programs/selective-harvest.py
+++ programs/selective-harvest.py
@@ -1,10 +1,46 @@
+#!/usr/bin/env python3
+
+# AUTHOR:
+#
+# Dirk Roorda, DANS, NL, dirk.roorda@dans.knaw.nl
+#
+# USAGE
+#
+# ./selective-harvest.py --help
+#
+# or
+#
+# python3 selective-harvest.py --help
+#
+# will show complete usage information.
+#
+# Shortest form:
+#
+# ./selective-harvest.py
+#
+# assuming that the config file is in config.xml
+#
+# Usual form
+#
+# ./selective-harvest.py -w work-directory -c config-file -l logfile
+#
+# Verbosity can be increased with -v, -vv, -vvv.
+#
+# All messages go to log file regardless of verbosity.
+
 import sys
 import os
+import time
+import datetime
 import re
 import argparse
 from subprocess import run
 import xml.etree.ElementTree as ET
 
+
+LOG = None
+
+TIMESTAMP = time.time()
 
 VERBOSE = False
 
@@ -14,43 +50,84 @@ metadataPat = re.compile('<metadata[^>]*>(.*)</metadata>', re.S)
 errorPat = re.compile('''<error.*code=['"]([^'"]*)['"][^>]*>(.*)</error>''', re.S)
 
 
-def error(msg):
-  if VERBOSE > 0:
-    sys.stderr.write(msg)
+def timestamp():
+  now = time.time()
+  interval = now - TIMESTAMP
+  if interval < 10:
+    intervalRep = "{: 2.2f}s".format(interval)
+  else:
+    interval = int(round(interval))
+    if interval < 60:
+      intervalRep = "{:>2d}s".format(interval)
+    elif interval < 3600:
+      intervalRep = "{:>2d}m {:>02d}s".format(interval // 60, interval % 60)
+    else:
+      intervalRep = "{:>2d}h {:>02d}m {:>02d}s".format(
+          interval // 3600, (interval % 3600) // 60, interval % 60
+      )
+  return '{} = {}'.format(datetime.datetime.now().isoformat(), intervalRep)
 
 
-def errorln(msg):
-  if VERBOSE > 0:
-    sys.stderr.write('{}\n'.format(msg))
+def _msg(msg, verbosity, newline, time, log, term):
+  """
+  Print a message msg if the verbosity admits it.
+  optionally with newline, optionally with timestamp,
+  optionally to a log file including an optional timestamp,
+  Writes to log file regardless of verbosity.
+  """
+  nl = '\n' if newline else ''
+  if term and VERBOSE > verbosity:
+    sys.stderr.write('{}{}'.format(msg, nl))
+  if log and LOG is not None:
+    ts = '{} > '.format(timestamp()) if time else ''
+    LOG.write('{}{}{}'.format(ts, msg, nl))
 
 
-def info(msg):
-  if VERBOSE > 1:
-    sys.stderr.write(msg)
+def shout(msg):
+  _msg(msg, -100, False, False, False, True)
 
 
-def infoln(msg):
-  if VERBOSE > 1:
-    sys.stderr.write('{}\n'.format(msg))
+def shoutln(msg):
+  _msg(msg, -100, True, False, False, True)
 
 
-def extra(msg):
-  if VERBOSE > 2:
-    sys.stderr.write(msg)
+def error(msg, time=True, log=True, term=True):
+  _msg(msg, 0, False, time, log, term)
 
 
-def extraln(msg):
-  if VERBOSE > 2:
-    sys.stderr.write('{}\n'.format(msg))
+def errorln(msg, time=True, log=True, term=True):
+  _msg(msg, 0, True, time, log, term)
 
 
-def readTask(config, selectRepos):
-  if not os.path.exists(config):
-    errorln('No config file "{}"'.format(config))
+def info(msg, time=True, log=True, term=True):
+  _msg(msg, 1, False, time, log, term)
+
+
+def infoln(msg, time=True, log=True, term=True):
+  _msg(msg, 1, True, time, log, term)
+
+
+def extra(msg, time=True, log=True, term=True):
+  _msg(msg, 2, False, time, log, term)
+
+
+def extraln(msg, time=True, log=True, term=True):
+  _msg(msg, 2, True, time, log, term)
+
+
+def readTasks(configPath, selectRepos):
+  """
+  Read an XML config file, and convert it to a tasks list.
+  Each task specifies a repository with pseudo sets and ids in
+  those sets to harvest, plus a location where the harvested
+  documents should end up.
+  """
+  if not os.path.exists(configPath):
+    errorln('No config file "{}"'.format(configPath))
     return False
-  info('Reading config file "{}" ...'.format(config))
-  tree = ET.parse(config)
-  infoln('done')
+  info('Reading config file "{}" ...'.format(configPath))
+  tree = ET.parse(configPath)
+  infoln('done', time=False)
 
   repos = []
 
@@ -81,20 +158,36 @@ def readTask(config, selectRepos):
       repoInfo['sets'].append(setInfo)
     repos.append(repoInfo)
 
-  extraln(repos)
+  for repo in repos:
+    extraln('{} => {}'.format(
+        repo.get('name', 'UNKNOWN REPO'),
+        repo.get('dest'),
+    ))
+    for pset in repo.get('sets', set()):
+      extraln('\t{}'.format(
+          pset.get('name', 'UNKNOWN SET'),
+      ))
+      for did in sorted(pset.get('ids', set())):
+        extraln('\t\t{}'.format(did))
   return repos
 
 
 def harvestAll(repoTasks):
+  """
+  Execute all harvesting tasks.
+  """
   good = True
   for repoTask in repoTasks:
-    thisGood = harvest(repoTask)
+    thisGood = harvestTask(repoTask)
     if not thisGood:
       good = False
   return good
 
 
-def harvest(repoTask):
+def harvestTask(repoTask):
+  """
+  Execute a single harvesting task.
+  """
   taskName = repoTask.get('name', 'UNSPECIFIED')
   infoln('Harvesting from "{}"'.format(taskName))
   dest = repoTask.get('dest', '')
@@ -138,7 +231,8 @@ def harvest(repoTask):
       docError = None
       repoUrl = repoTask.get('url', '')
       meta = repoTask.get('meta', '')
-      info('\t\tharvesting "{:<40}" ... '.format(docId))
+      msg = '    harvesting "{}" ... '.format(docId)
+      info(msg)
       docUrl = '{}?verb=GetRecord&identifier={}&metadataPrefix={}'.format(
           repoUrl,
           docId,
@@ -159,7 +253,14 @@ def harvest(repoTask):
       if docError:
         setGood = False
         nError += 1
-      infoln('XX' if docError else 'OK')
+      if docError:
+        if VERBOSE <= 1:
+          errorln('{}XX'.format(msg), log=False)
+          errorln('XX', time=False, term=False)
+        else:
+          errorln('XX', time=False)
+      else:
+        infoln('OK', time=False)
       if docError:
         docError = str(docError).rstrip('\n')
         infoln('\t\t\t{}'.format(docError))
@@ -172,32 +273,72 @@ def harvest(repoTask):
 
 
 def deliver(path):
-    with open(path) as fh:
-      text = fh.read()
-    error = None
-    if '</GetRecord>' in text and '</metadata>' in text:
-        match = metadataPat.search(text)
-        if match:
-            text = match.group(1).strip()
-            with open(path, 'w') as fh:
-              fh.write(text)
-        else:
-            error = 'No metadata found'
-    elif '</error>' in text:
-        match = errorPat.search(text)
-        if match:
-            code = match.group(1)
-            msg = match.group(2)
-            error = f'{code}: {msg}'
-        else:
-            error = 'Could not parse error message'
-    else:
-        error = 'No record found and no error message found'
-    return error
+  """
+  harvestTask writes the result of a harvest request to disk as is.
+  This function feels the OAI-PMH wrapper off the document, and saves
+  the document in the same place.
+  """
+  with open(path) as fh:
+    text = fh.read()
+  error = None
+  if '</GetRecord>' in text and '</metadata>' in text:
+      match = metadataPat.search(text)
+      if match:
+          text = match.group(1).strip()
+          with open(path, 'w') as fh:
+            fh.write(text)
+      else:
+          error = 'No metadata found'
+  elif '</error>' in text:
+      match = errorPat.search(text)
+      if match:
+          code = match.group(1)
+          msg = match.group(2)
+          error = f'{code}: {msg}'
+      else:
+          error = 'Could not parse error message'
+  else:
+      error = 'No record found and no error message found'
+  return error
 
 
 def main():
+  """
+  Sets up an argument parser.
+  Changes to the working directory which is specified in the -w argument.
+  Starts appending to the log file.
+  Reads the config file which is specified in the -c argument.
+  Harvests all repos and document-ids found in the config file,
+  unless -r repo is given, in which case only document-ids from
+  repo are harvested.
+  The harvested documents are stored in a location specified
+  per repo in the config file.
+
+  Example config file:
+
+    <config>
+        <repository id="dans-easy">
+            <baseurl>https://easy.dans.knaw.nl/oai/</baseurl>
+            <metadataprefix>oai_dc</metadataprefix>
+            <output-set name="theo1">
+                <id>oai:easy.dans.knaw.nl:easy-dataset:4215</id>
+                <id>oai:easy.dans.knaw.nl:easy-dataset:25037</id>
+                <id>oai:easy.dans.knaw.nl:easy-dataset:25037x</id>
+            </output-set>
+            <output-set name="theo2">
+                <id>oai:easy.dans.knaw.nl:easy-dataset:30678</id>
+                <id>oai:easy.dans.knaw.nl:easy-dataset:32044</id>
+            </output-set>
+            <from>2006-11-01T00:00:00Z</from>
+            <recordpath>_temp/oai-pmh-harvester/dans</recordpath>
+        </repository>
+    </config>
+
+  Run ./selective-harvest.py --help to see complete usage information.
+  """
+
   global VERBOSE
+  global LOG
 
   parser = argparse.ArgumentParser(description='selective harvest arguments')
   parser.add_argument(
@@ -206,9 +347,18 @@ def main():
       help='path to config file (xml)',
   )
   parser.add_argument(
+      '-l', '--log',
+      default='log.txt',
+      help='path to log file',
+  )
+  parser.add_argument(
       '-w', '--workdir',
       default='',
-      help='path to working directory',
+      help=(
+          'path to working directory which is'
+          'the starting point of a relative config path'
+          'and the starting point of a relative output path'
+      ),
   )
   parser.add_argument(
       '-r', '--repo',
@@ -220,13 +370,30 @@ def main():
       '-v', '--verbose',
       action='count',
       default=0,
-      help='print messages',
+      help=(
+          'print errors, messages, verbose messages.'
+          'Repeat the option to increase verbosity.'
+      ),
   )
   args = parser.parse_args()
   VERBOSE = args.verbose
 
   workDir = os.path.abspath(args.workdir)
   os.chdir(workDir)
+
+  logPath = os.path.abspath(args.log)
+  logDir = os.path.dirname(logPath)
+  if not os.path.exists(logDir):
+    try:
+      os.makedirs(logDir, exist_ok=True)
+    except Exception:
+      shoutln('Cannot create log directory "{}"'.format(logDir))
+  try:
+    LOG = open(logPath, 'a')
+  except Exception:
+    LOG = None
+    shoutln('Cannot write to log file "{}"'.format(logPath))
+
   infoln('working in directory "{}"'.format(os.getcwd()))
 
   repos = None if args.repo == '' else set(args.repo.split(','))
@@ -234,7 +401,7 @@ def main():
     infoln('Harvest all repos found in "{}"'.format(args.config))
   else:
     infoln('Harvest repositories "{}" only'.format('", "'.join(repos)))
-  repoTasks = readTask(args.config, repos)
+  repoTasks = readTasks(args.config, repos)
   if not repoTasks:
     return 1
   if not harvestAll(repoTasks):
@@ -242,4 +409,7 @@ def main():
   return 0
 
 
-sys.exit(main())
+returnCode = main()
+if returnCode != 0:
+  shoutln('ERROR {}'.format(returnCode))
+sys.exit(returnCode)
